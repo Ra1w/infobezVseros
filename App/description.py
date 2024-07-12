e5f2d8121b927e7b9d07d6296a7dad6c48a3a7d1
Ra1w <ArtyomCh2007@yandex.ru>
2024-07-12 19:02:11 +0400

    Add files via upload

---
+++ App/description.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtGui import QFont
+
+class Description(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: {}".format('#ffffff'))
+
+        layout = QVBoxLayout(self)
+        self.label = QLabel()
+        self.label.setText("Эта программа созданна для того, чтобы быть уничтоженной")
+        self.label.setStyleSheet("background-color: {}".format('#828cae'))
+        self.label.setFont(QFont("Times", 14))
+        layout.addWidget(self.label, 0)
