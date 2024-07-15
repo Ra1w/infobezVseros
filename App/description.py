af180773ea6cc209e92133504c86ccec470b4bee
Ra1w <ArtyomCh2007@yandex.ru>
2024-07-15 17:48:52 +0400

    Update description.py

--- App/description.py
+++ App/description.py
@@ -8,7 +8,11 @@ class Description(QWidget):
 
         layout = QVBoxLayout(self)
         self.label = QLabel()
-        self.label.setText("Эта программа созданна для того, чтобы уничтожить человечество!\n [злобный смех]\n\n\n\n\n\n\n\n")
+        self.label.setText("     Стеганография - это способ передачи или хранения информации с учётом \n"
+                           "сохранения в тайне самого факта такой передачи (хранения).\n"
+                            "       Данное приложение служит для реализации принципов стеганографии изображений\n"
+                           "Приложение позволяет прятать текст в изображении формата *.png, а также \n"
+                           "и находить такой текст в изображении.")
         self.label.setStyleSheet("background-color: {}".format('#828cae'))
         self.label.setFont(QFont("Times", 14))
         layout.addWidget(self.label, 0)
