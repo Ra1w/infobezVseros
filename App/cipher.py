01d840a6d67aa2377ce6e0a42db4e10a6e9464d8
Ra1w <ArtyomCh2007@yandex.ru>
2024-07-16 14:51:06 +0400

    Update cipher.py

--- App/cipher.py
+++ App/cipher.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from random import seed, randint
 
 class Blue_changing:
     def encrypt(message, file_path):
@@ -6,22 +7,39 @@ class Blue_changing:
         pixels = image.load()
         size = len(message)
         x, y = image.size
-        for i in range(min(size, x*y)):
-            t = pixels[i % x, i // x]
+        m = pixels[0, 0]
+        seed(m[0]*1000000 + m[1]*1000 + m[2])
+        mas = []
+        for i in range(x):
+            for j in range(y):
+                mas.append([i, j])
+        mas.pop(0)
+        for i in range(min(size, x*y - 1, 80000)):
+            t1 = randint(0, len(mas) - 1)
+            t = pixels[mas[t1][0], mas[t1][1]]
             sim = ord(message[i])
             if sim > 1023:
                 sim -= 896
-            pixels[i % x, i // x] = (t[0], t[1], sim)
+            pixels[mas[t1][0], mas[t1][1]] = (t[0], t[1], sim)
+            mas.pop(t1)
         image.save(file_path)
     def decrypt(file_path):
         image = Image.open(file_path)
         pixels = image.load()
         answer = ""
         x, y = image.size
-        for j in range(y):
-            for i in range(x):
-                sim = pixels[i, j][2]
-                if sim > 127:
-                    sim += 896
-                answer += chr(sim)
+        m = pixels[0, 0]
+        seed(m[0] * 1000000 + m[1] * 1000 + m[2])
+        mas = []
+        for i in range(x):
+            for j in range(y):
+                mas.append([i, j])
+        mas.pop(0)
+        for i in range(min(x*y - 1, 80000)):
+            t1 = randint(0, len(mas) - 1)
+            sim = pixels[mas[t1][0], mas[t1][1]][2]
+            if sim > 127:
+                sim += 896
+            answer += chr(sim)
+            mas.pop(t1)
         return answer
